5e266389818f4a1a1c6bca1b716cd9ee977f8aa3
Siggerud <christian.siggerud@gmail.com>
2025-03-13 19:46:53 +0100

    debug

--- src/stabilizer.py
+++ src/stabilizer.py
@@ -51,12 +51,11 @@ class Stabilizer(RobotProcess):
             if (self._pca9685.get_servo_angle(self._stabilizerChannels["frontRight"]) + 1) > 180 and (self._pca9685.get_servo_angle(self._stabilizerChannels["rearRight"]) -1) < 0:
                 self._pca9685.set_servo_to_angle(self._stabilizerChannels["frontLeft"], self._pca9685.get_servo_angle(self._stabilizerChannels["frontLeft"]) + 1)
                 self._pca9685.set_servo_to_angle(self._stabilizerChannels["rearLeft"], self._pca9685.get_servo_angle(self._stabilizerChannels["rearLeft"]) - 1)
-
+                print(int(self._pca9685.get_servo_angle(self._stabilizerChannels["frontLeft"])))
+                print(int(self._pca9685.get_servo_angle(self._stabilizerChannels["rearLeft"])))
             else:
                 self._pca9685.set_servo_to_angle(self._stabilizerChannels["frontRight"], self._pca9685.get_servo_angle(self._stabilizerChannels["frontRight"]) +1)
                 self._pca9685.set_servo_to_angle(self._stabilizerChannels["rearRight"], self._pca9685.get_servo_angle(self._stabilizerChannels["rearRight"]) -1)
-            print(self._pca9685.get_servo_angle(self._stabilizerChannels["frontRight"]))
-            print(self._pca9685.get_servo_angle(self._stabilizerChannels["rearRight"]))
             if self._overRollTreshold == False:
                 print("Roll angle is too high")
                 self._overRollTreshold = True

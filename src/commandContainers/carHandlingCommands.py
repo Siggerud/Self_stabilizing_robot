9e8f5aee07f7a76110c2709016ac54cc25152a10
Siggerud <christian.siggerud@gmail.com>
2025-03-23 16:50:48 +0100

    added hash method

--- src/commandContainers/carHandlingCommands.py
+++ src/commandContainers/carHandlingCommands.py
@@ -6,3 +6,13 @@ class CarHandlingCommand:
     movement: Optional[str] = None
     speedValue: Optional[int] = None
     speedChange: Optional[int] = None
+
+    def __eq__(self, other):
+        if isinstance(other, CarHandlingCommand):
+            return (self.movement == other.movement and
+                    self.speedChange == other.speedChange and
+                    self.speedValue == other.speedValue)
+        return False
+
+    def __hash__(self):
+        return hash((self.movement, self.speedChange, self.speedValue))

6fb2e97f0bebf9cdc4c425e1641d4975a7626a33
Siggerud <christian.siggerud@gmail.com>
2025-03-24 19:37:26 +0100

    added test

--- test/testVoiceCommandHandler.py
+++ test/testVoiceCommandHandler.py
@@ -10,7 +10,7 @@ from exceptions import InvalidCommandException
 def voiceHandler():
     return VoiceCommandHandler()
 
-def test_get_camera_helper_commands(voiceHandler):
+def test_get_camera_helper_commands_param_check(voiceHandler):
     commands: dict[str: str] = {
         "zoom": "zoom {notParam}",
         "turn_on_display": "a",
@@ -20,6 +20,20 @@ def test_get_camera_helper_commands(voiceHandler):
     }
     with pytest.raises(InvalidCommandException):
         voiceHandler.get_camera_helper_commands(commands, 1.0, 2.0, 0.2)
+
+def test_get_car_handling_commands_param_check(voiceHandler):
+    commands: dict[str: str] = {
+        "go_forward": "go forward",
+        "go_backward": "go backward",
+        "turn_left": "turn left",
+        "turn_right": "turn right",
+        "stop": "stop",
+        "increase_speed": "increase speed",
+        "decrease_speed": "increase speed",
+        "set_speed": "set speed {param}"
+    }
+    with pytest.raises(InvalidCommandException):
+        voiceHandler.get_car_handling_commands(commands, 5, 10, 100)
 
 
 

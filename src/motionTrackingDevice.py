af619973e960adc48d8062d41ef91e559a1d3fc9
Siggerud <christian.siggerud@gmail.com>
2025-04-04 21:42:52 +0200

    added some debug prints

--- src/motionTrackingDevice.py
+++ src/motionTrackingDevice.py
@@ -119,3 +119,6 @@ class MotionTrackingDevice:
 
         self._offsetRoll = offsets[self._rollAxis]
         self._offsetPitch = offsets[self._pitchAxis]
+
+        print(self._offsetRoll)
+        print(self._offsetPitch)

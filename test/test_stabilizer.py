596f8d3b5c2cfc89ea63c9520edab1b20d0a2bf5
Siggerud <christian.siggerud@gmail.com>
2025-04-13 15:34:59 +0200

    added tests

--- test/test_stabilizer.py
+++ test/test_stabilizer.py
@@ -9,10 +9,10 @@ from stabilizer import Stabilizer
 from exceptions import StabilizerException
 
 @pytest.mark.parametrize("test_input",
-                         [[0, 1, 3, 1]],
+                         [[0, 1, 3, 1],
                          [1, 1, 1, 1],
                          [0, 1, 2, 16],
-                         [20, 0, 1, 2])
+                         [-1, 0, 1, 2]])
 def test_validate_input_raise_error_on_channel_input(test_input):
     pca9695 = Mock()
     motionTrackingDevice = Mock()

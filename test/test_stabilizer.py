cde80163654a7409ab94290ddee609c9895f70c2
Siggerud <christian.siggerud@gmail.com>
2025-04-13 15:38:33 +0200

    added tests

--- test/test_stabilizer.py
+++ test/test_stabilizer.py
@@ -9,6 +9,25 @@ from stabilizer import Stabilizer
 from exceptions import StabilizerException
 
 @pytest.mark.parametrize("test_input",
+                         [[0, 91],
+                         [91, 0],
+                          [-5, 1],
+                          [-5, 5],
+                          [100, 92]])
+def test_validate_input_raise_error_on_tresholds(test_input):
+    pca9695 = Mock()
+    motionTrackingDevice = Mock()
+    channels = {
+        "frontLeft": 0,
+        "rearLeft": 1,
+        "frontRight": 2,
+        "rearRight": 3}
+    rollTreshold, pitchTreshold = test_input
+
+    with pytest.raises(StabilizerException):
+        Stabilizer(motionTrackingDevice, pca9695, rollTreshold, pitchTreshold, channels)
+
+@pytest.mark.parametrize("test_input",
                          [[0, 1, 3, 1],
                          [1, 1, 1, 1],
                          [0, 1, 2, 16],

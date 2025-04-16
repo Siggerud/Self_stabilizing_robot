5a0927e65e38ee9221e57b2eac420d7861941d18
Siggerud <christian.siggerud@gmail.com>
2025-04-16 21:05:49 +0200

    debug

--- src/xboxControl.py
+++ src/xboxControl.py
@@ -47,7 +47,9 @@ class XboxControl:
         }
 
     def get_controller_data(self) -> list[XBoxControlData]:
+        print("before loop")
         while True:
+            print("after loop")
             events = self._get_controller_events()
             if len(events) > 0:
                 return [self._get_xbox_control_data(event) for event in events]

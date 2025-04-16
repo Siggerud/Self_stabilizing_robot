04d129cc4b72f672d923a949ebe02680f051e3f8
Siggerud <christian.siggerud@gmail.com>
2025-04-16 21:04:51 +0200

    debug

--- src/xBoxControlHandler.py
+++ src/xBoxControlHandler.py
@@ -20,6 +20,7 @@ class XBoxControlHandler(CommandGenerator):
     def process_commands(self, flag) -> None:
         print("Starting!")
         while not flag.value:
+            print("now!")
             controllerData = self._xboxControl.get_controller_data()
 
             commands = self._process_controller_data_to_commands(controllerData)

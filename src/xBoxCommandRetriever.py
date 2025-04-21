032cfebfbd0efe264014395a0cb1d868cd5e8cc7
Siggerud <christian.siggerud@gmail.com>
2025-04-21 20:54:47 +0200

    debug

--- src/xBoxCommandRetriever.py
+++ src/xBoxCommandRetriever.py
@@ -48,7 +48,7 @@ class XBoxCommandRetriever(CommandRetriever):
             "Y press": CameraHelperCommand(changeDisplayActive=True)
         }
 
-
+        print(stepValue)
         for stickValue in [round(float(x), 2) for x in np.arange(-1, 0 + stepValue, stepValue)]:
             stickValueToZoomValue = int(map_value_to_new_scale(stickValue, 0, -1, minZoomValue, maxZoomValue))
             commands[f"RSB vertical {stickValue}"] = stickValueToZoomValue

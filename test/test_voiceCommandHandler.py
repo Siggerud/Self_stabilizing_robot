9842d32ef48f30d11dae3a27918643815ed765df
Siggerud <christian.siggerud@gmail.com>
2025-04-27 19:46:26 +0200

    updated test

--- test/test_voiceCommandHandler.py
+++ test/test_voiceCommandHandler.py
@@ -3,13 +3,13 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 import pytest
-from voiceCommandRetriever import VoiceCommandRetriever
+from voiceCommandMapper import VoiceCommandMapper
 from exceptions import InvalidCommandException
 from data.commandContainers.carHandlingCommands import CarHandlingCommand
 
 @pytest.fixture
 def voiceHandler():
-    return VoiceCommandRetriever()
+    return VoiceCommandMapper()
 
 def test_command_descriptions(voiceHandler):
     commands = {"forward_command": "go forward",

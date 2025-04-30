7791920e481a9fba8232526dbe0930a301d319c2
Siggerud <christian.siggerud@gmail.com>
2025-04-30 22:33:43 +0200

    added test

--- test/test_xboxCommandMapper.py
+++ test/test_xboxCommandMapper.py
@@ -7,11 +7,31 @@ import pytest
 from exceptions import InvalidCommandException
 from xBoxCommandMapper import XBoxCommandMapper
 from data.commandContainers.honkCommands import HonkCommand
+from data.commandContainers.cameraHelperCommands import CameraHelperCommand
 
 
 @pytest.fixture
 def commandMapper():
     return XBoxCommandMapper()
+
+
+@pytest.mark.parametrize("spec_input,key,value", [
+    ({"zoom": {"zoom_step": 0.1},
+         "xbox": {"commands": {
+        "turn_display_on_or_off": "b",
+        "zoom_in": "d-pad up",
+        "zoom_out": "d-pad down"}}}, "B press", CameraHelperCommand(changeDisplayActive=True)),
+    ({"zoom": {"zoom_step": 0.2},
+      "xbox": {"commands": {
+        "turn_display_on_or_off": "y",
+        "zoom_in": "D-pad left",
+        "zoom_out": "D-pad right"}}}, "D-PAD left", CameraHelperCommand(zoomChange=0.2)),
+])
+def test_camera_helper_commands(commandMapper, spec_input, key, value):
+    result: dict = commandMapper.get_camera_helper_commands(spec_input)
+
+    assert result[key] == value
+
 
 @pytest.mark.parametrize("spec_input,key,value", [
     ({"xbox": {"commands": {"honk": "x"}}}, "X press", HonkCommand(startContinuousHonk=True)),
@@ -22,6 +42,7 @@ def test_get_honk_commands(commandMapper, spec_input, key, value):
 
     assert result[key] == value
 
+
 @pytest.mark.parametrize("spec_input,expected_output", [
     ({"xbox": {"commands": {"exit": "y"}}}, "Y press"),
     ({"xbox": {"commands": {"exit": "Back"}}}, "BACK press")
@@ -30,6 +51,7 @@ def test_exit_command(commandMapper, spec_input, expected_output):
     result: str = commandMapper.get_exit_command(spec_input)
 
     assert result == expected_output
+
 
 @pytest.mark.parametrize("test_input", [
     {"xbox": {"commands": {"exit": "rt"}}},
@@ -80,6 +102,7 @@ def test_dpad_button_validity_check(commandMapper, test_input):
 def test_sticks_validity_check(commandMapper, test_input):
     with pytest.raises(InvalidCommandException):
         commandMapper.get_camera_servo_handling_commands(test_input)
+
 
 @pytest.mark.parametrize("test_input", [
     {"xbox": {"commands": {

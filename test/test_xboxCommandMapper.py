b5a2f2c57348a3e2020405526a1a4cb15b1473a1
Siggerud <christian.siggerud@gmail.com>
2025-04-30 21:28:34 +0200

    added test

--- test/test_xboxCommandMapper.py
+++ test/test_xboxCommandMapper.py
@@ -7,27 +7,49 @@ import pytest
 from exceptions import InvalidCommandException
 from xBoxCommandMapper import XBoxCommandMapper
 
+
 @pytest.fixture
 def commandMapper():
     return XBoxCommandMapper()
 
+
 @pytest.mark.parametrize("test_input", [
     {"xbox": {"commands": {"exit": "rt"}}},
-{"xbox": {"commands": {"exit": "D-PAD up"}}},
-{"xbox": {"commands": {"exit": "lsb"}}}
+    {"xbox": {"commands": {"exit": "D-PAD up"}}},
+    {"xbox": {"commands": {"exit": "lsb"}}}
 ])
 def test_push_button_validity_check(commandMapper, test_input):
     with pytest.raises(InvalidCommandException):
         commandMapper.get_exit_command(test_input)
 
-def test_dpad_button_validity_check():
-    pass
-    #TODO
+
+@pytest.mark.parametrize("test_input", [
+    {"xbox": {"commands": {
+        "turn_display_on_or_off": "A",
+        "zoom_in": "RT",
+        "zoom_out": "DRAD down"
+    }}},
+    {"xbox": {"commands": {
+        "turn_display_on_or_off": "A",
+        "zoom_in": "D-PAD up",
+        "zoom_out": "x"
+    }}},
+    {"xbox": {"commands": {
+        "turn_display_on_or_off": "A",
+        "zoom_in": "START",
+        "zoom_out": "D-PAD left"
+    }}}
+])
+def test_dpad_button_validity_check(commandMapper, test_input):
+    with pytest.raises(InvalidCommandException):
+        commandMapper.get_camera_helper_commands(test_input)
+
 
 def test_sticks_validity_check():
-    #TODO
+    # TODO
     pass
 
+
 def test_trigger_button_validity_check():
-    #TODO
+    # TODO
     pass

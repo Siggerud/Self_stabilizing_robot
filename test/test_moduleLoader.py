d72e77967874a00d7f5c62456c951d5de6a49fd5
Siggerud <christian.siggerud@gmail.com>
2025-05-11 21:18:53 +0200

    updated test

--- test/test_moduleLoader.py
+++ test/test_moduleLoader.py
@@ -5,15 +5,16 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 
 import pytest
 from moduleLoader import ModuleLoader
-from audioHandler import AudioHandler
+#from audioHandler import AudioHandler
 from unittest.mock import patch, MagicMock
 
 @pytest.fixture
 def loader():
     return ModuleLoader()
 
-@patch('audioHandler.AudioHandler')
+@patch('moduleLoader.AudioHandler')
 def test_setup_command_generator(mock_audio_handler, loader):
+    #print(AudioHandler is mock_audio_handler)
     mock_audio_handler_instance = MagicMock()
     mock_audio_handler.return_value = mock_audio_handler_instance
 

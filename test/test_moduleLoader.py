910c8a8ea1dfc67828c22ce2809b393a24b7112a
Siggerud <christian.siggerud@gmail.com>
2025-05-11 21:06:24 +0200

    updated test

--- test/test_moduleLoader.py
+++ test/test_moduleLoader.py
@@ -13,9 +13,11 @@ def loader():
     return ModuleLoader()
 
 @patch('moduleLoader.AudioHandler')
-def test_setup_command_generator(mock_audio_handler):
+def test_setup_command_generator(loader, mock_audio_handler):
     # Arrange
     mock_audio_handler_instance = MagicMock()
     mock_audio_handler.return_value = mock_audio_handler_instance
+
+    loader.setup_command_generator()
 
     assert mock_audio_handler.assert_called_once()

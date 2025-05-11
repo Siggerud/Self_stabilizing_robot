3bd2d59b86c1d6d2cb9a11ab66ef7727bfec65b2
Siggerud <christian.siggerud@gmail.com>
2025-05-11 20:53:14 +0200

    added test for module loader with config files

--- test/test_moduleLoader.py
+++ test/test_moduleLoader.py
@@ -2,3 +2,16 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from moduleLoader import ModuleLoader
+from audioHandler import AudioHandler
+import pytest
+
+@pytest.fixture
+def loader():
+    return ModuleLoader()
+
+def test_setup_command_generator(loader):
+    result = loader.setup_command_generator()
+
+    assert isinstance(result, AudioHandler)

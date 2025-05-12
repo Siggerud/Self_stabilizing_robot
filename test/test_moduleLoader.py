07a85aa79c14118b7e82ec26d5b6fa1c687babc4
Siggerud <christian.siggerud@gmail.com>
2025-05-12 20:18:08 +0200

    added module loader test

--- test/test_moduleLoader.py
+++ test/test_moduleLoader.py
@@ -14,13 +14,23 @@ def configDirPath():
     return path.join(path.dirname(__file__), "config")
 
 @patch('moduleLoader.AudioHandler')
-def test_setup_command_generator(mock_audio_handler, configDirPath):
-    print(configDirPath)
-    loader = ModuleLoader(configDirPath, "global")
+def test_setup_command_generator_audio(mock_audio_handler, configDirPath):
+    loader = ModuleLoader(configDirPath, "global_audio")
 
-    mock_audio_handler_instance = MagicMock()
-    mock_audio_handler.return_value = mock_audio_handler_instance
+    #mock_audio_handler_instance = MagicMock()
+    #mock_audio_handler.return_value = mock_audio_handler_instance
 
     loader.setup_command_generator()
 
     mock_audio_handler.assert_called_once()
+
+@patch('moduleLoader.XboxEventHandler')
+def test_setup_command_generator_xbox(mock_xbox_handler, configDirPath):
+    loader = ModuleLoader(configDirPath, "global_xbox")
+
+    #mock_audio_handler_instance = MagicMock()
+    #mock_xbox_handler.return_value = mock_audio_handler_instance
+
+    loader.setup_command_generator()
+
+    mock_xbox_handler.assert_called_once()

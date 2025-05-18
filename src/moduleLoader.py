102a7741b9e077a796e57240a8081d6e4cba4620
Siggerud <christian.siggerud@gmail.com>
2025-05-18 13:59:29 +0200

    refactored moduleloader

--- src/moduleLoader.py
+++ src/moduleLoader.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-from motionTrackingDevice import MotionTrackingDevice
 from utility.yamlParser import get_yaml_content_from_file, get_float, get_int, get_bool
 from audioHandler import AudioHandler
 from camera import Camera

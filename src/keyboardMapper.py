c875fc2d5e58d3d8ded6e6c5465887fb3c986dfe
Siggerud <christian.siggerud@gmail.com>
2025-05-24 18:24:19 +0200

    debug

--- src/keyboardMapper.py
+++ src/keyboardMapper.py
@@ -35,8 +35,18 @@ class KeyboardMapper(CommandMapperBase):
     def get_camera_servo_handling_commands(self, *args) -> dict:
         pass
 
-    def get_command_descriptions(self, *args) -> dict:
-        pass
+    def get_command_descriptions(self, specs) -> dict:
+        #TODO: move this to helper class
+        commands: dict[str: str] = specs["keyboard"]["commands"]
+        descriptions: dict[str: str] = specs["keyboard"]["command_descriptions"]
+
+        # match the commands with their descriptions
+        commandsToDescriptions: dict[str: str] = {commandValue: descValue for
+                                                  (commandKey, commandValue, descKey, descValue) in
+                                                  zip(commands.keys(), commands.values(), descriptions.keys(),
+                                                      descriptions.values()) if commandKey == descKey}
+
+        return commandsToDescriptions
 
     def get_camera_helper_commands(self, *args) -> dict:
         pass

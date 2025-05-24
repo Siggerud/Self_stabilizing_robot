386ef6f1b9fdef4056a40f1475b0b2e3dce740d2
Siggerud <christian.siggerud@gmail.com>
2025-05-24 17:56:09 +0200

    experimenting

--- src/commandHandler.py
+++ src/commandHandler.py
@@ -54,6 +54,7 @@ class CommandHandler(RobotProcess):
             roboObject.cleanup()
 
     def execute_commands(self, flag, shared_array) -> None:
+        print("hey")
         while not flag.value:
             command: str = self._pipeReceiver.recv()
             print(command)

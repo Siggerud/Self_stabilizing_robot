50a02072ab0fbd34f11c6b3fddaeb54e1eba93fb
Siggerud <christian.siggerud@gmail.com>
2025-05-27 20:33:57 +0200

    debug

--- src/interProcessCommunicationObjectLoader.py
+++ src/interProcessCommunicationObjectLoader.py
@@ -1,6 +1,7 @@
 from multiprocessing import Array, Pipe
 from utility.yamlParser import get_yaml_content_from_file, get_bool
 from typing import Optional
+from os import path
 
 class InterProcessCommunicationObjectLoader:
     def __init__(self, configDirPath: str):
@@ -13,7 +14,7 @@ class InterProcessCommunicationObjectLoader:
 
     def load_shared_array_between_camera_and_command_handler(self, cameraConfigFileName: str, carConfigFileName: str, servoConfigFileName: str) -> Optional[Array]:
         print(self._configDirPath)
-        cameraSpecs = get_yaml_content_from_file(cameraConfigFileName)
+        cameraSpecs = self._get_content_from_config_file(cameraConfigFileName)
         if not self._check_if_module_enabled(cameraSpecs):
             return None
 
@@ -23,12 +24,12 @@ class InterProcessCommunicationObjectLoader:
         arrayList[0] = 1.0
         arrayList[1] = 1.0
 
-        carSpecs = get_yaml_content_from_file(carConfigFileName)
+        carSpecs = self._get_content_from_config_file(carConfigFileName)
         if self._check_if_module_enabled(carSpecs):
             arrayList.append(0.0) # speed
             arrayList.append(0.0) # direction
 
-        servoSpecs = get_yaml_content_from_file(servoConfigFileName)
+        servoSpecs = self._get_content_from_config_file(servoConfigFileName)
         if self._check_if_module_enabled(servoSpecs):
             arrayList.append(0.0) # horizontal servo angle
             arrayList.append(0.0) # vertical servo angle
@@ -38,3 +39,9 @@ class InterProcessCommunicationObjectLoader:
     #TODO: move to helper class
     def _check_if_module_enabled(self, specs: dict) -> bool:
         return get_bool(specs, "enabled")
+
+    # TODO: move to helper class
+    def _get_content_from_config_file(self, configFileName: str) -> dict:
+        absoluteFilePath: str = path.join(self._configDirPath, configFileName + '.yml')
+
+        return get_yaml_content_from_file(absoluteFilePath)

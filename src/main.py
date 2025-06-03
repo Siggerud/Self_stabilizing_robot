aa6deaa56c96e016e25fe2608c8d1db6ca890b55
Siggerud <christian.siggerud@gmail.com>
2025-06-03 20:02:31 +0200

    testing

--- src/main.py
+++ src/main.py
@@ -43,10 +43,10 @@ if __name__ == "__main__":
         print_error_message_and_exit(e)
 
     # start car
-    try:
-        carController.start()
-    except KeyboardInterrupt:
-        pass
+    print("before")
+    carController.start()
+    print("after")
+
 
     # send None to stop logger process
     queue.put(None)

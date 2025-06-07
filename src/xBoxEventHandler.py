9ba877ea6c75f2daa338725c7f9187d8eff52bb4
Siggerud <christian.siggerud@gmail.com>
2025-06-07 22:49:54 +0200

    debug

--- src/xBoxEventHandler.py
+++ src/xBoxEventHandler.py
@@ -62,6 +62,7 @@ class XBoxEventHandler(CommandGenerator):
         self._pipeSender.send(command)
 
     def _process_controller_data_to_commands(self, controllerData: XBoxControlData) -> str:
+        print(controllerData)
         return self._process_controller_data_to_command(controllerData) if controllerData is not None else ""
 
     def _process_controller_data_to_command(self, data: XBoxControlData) -> Optional[str]:
